caf97e636d4824d0cd939337ad3f9b7c75a912fb
iam-vijay-kiran <105585747+iam-vijay-kiran@users.noreply.github.com>
2024-08-21 10:34:54 +0530

    Add files via upload

---
+++ incremental_data_in_glue.py
@@ -0,0 +1,26 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+subsDf=glueContext.create_dynamic_frame.from_catalog(
+    database="telecom-glue-pro1-database",
+    table_name="telecom_data_glue_pro1",
+    transformation_ctx="s3_input_new")
+subsDf.printSchema()
+sparkSubsDf = subsDf.toDF()
+sparkSubsDf.show()
+print(sparkSubsDf.count())
+
+job.commit()
